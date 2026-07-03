4fc7df277f1ad1f5f9c46f0b3b84232514528747
Sadhan <sanandh@deloitte.com>
2026-07-03 10:37:14 +0530

    fix: angular

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: '/api',
   appName: 'ShopLite',
   googleClientId: '598332329716-havepistoah3bkcq992bb39h1ceoct8l.apps.googleusercontent.com'
 };

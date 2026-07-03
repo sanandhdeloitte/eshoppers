46ba9d1149e0d79a28d3da1de8e451f3e10c3454
Sadhan <sanandh@deloitte.com>
2026-07-03 10:40:15 +0530

    fix: angular

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   appName: 'ShopLite',
-  googleClientId: '598332329716-havepistoah3bkcq992bb39h1ceoct8l.apps.googleusercontent.com'
+  googleClientId: '598332329716-havepistoah3bkcq992bb39h1ceoct8l.apps.googleusercontent.com',
+  stripePublishableKey:  'pk_test_51To1uSHPHbfyKdsUv7axLukn6SnC5lNd2zMpyN7GT19IOfC29Exm9ScHRcCVrbJu8jaAflFz0fTL4myBLgg7ux5U00mGnm5TfN',
+
 };
